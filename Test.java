6c5a9bd974825a65fe0ecba74072f08eb89c1bb2
hniclarsen <48229001+hniclarsen@users.noreply.github.com>
2019-03-04 18:32:36 -0700

    Add files via upload

--- Test.java
+++ Test.java
@@ -1,16 +1,33 @@
 import java.util.List;
+import java.util.StringTokenizer;
 /**
  * Main function for testing programs within this folder.
  * @author    Heather N. Larsen
- * @version   1.0    2019/03/04    18:11MST
+ * @version   1.1    2019/03/04    18:32MST
  */
 public class Test {
     public static void main(String[] args) {
-        // Test primes(int,int) for PrimalityTester.java
-        ///*
-        PrimalityTester test = new PrimalityTester();
-        List primes = test.primes(1, 1000);
-        System.out.println(primes+"\nNumber of primes: "+primes.size());
-        //*/
+        String program  = args[0];
+        String function = args[1]; 
+        //PrimalityTester.java
+        if(program.compareTo("PrimalityTester")==0) {
+            primalityTester(function);
+        }
     }
+    //main(String[])============================================================
+    /**
+     * Test the functions in the PrimalityTester class.
+     * @param  fn  the function being tested
+     */
+    private static void primalityTester(String fn) {
+        PrimalityTester test = new PrimalityTester();
+        StringTokenizer tkn  = new StringTokenizer(fn,"(,)");
+        //primes(int,int)
+        if(tkn.nextToken().compareTo("primes")==0) {
+            List primes = test.primes(Integer.parseInt(tkn.nextToken()),
+                    Integer.parseInt(tkn.nextToken()));
+            System.out.println(primes+"\nNumber of primes: "+primes.size());
+        }
+    }
+    //primalityTester(String)===================================================
 }

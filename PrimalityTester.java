087694591c6b540cb4e4954daa70eed860b78931
Heather N. Larsen <48229001+hniclarsen@users.noreply.github.com>
2019-03-07 01:45:51 -0700

    Add files via upload

--- PrimalityTester.java
+++ PrimalityTester.java
@@ -15,7 +15,7 @@ public class PrimalityTester {
      * @param  hi  inclusive upper-bound integer
      * @return the list of primes between lo and hi
      */
-    public List primes(int lo, int hi) {
+    public List listPrimes(int lo, int hi) {
         // initialize a linked-list storage container for discovered primes
         List primes = Collections.synchronizedList(new LinkedList());
         // check each integer between val lo and hi for primality
@@ -28,22 +28,7 @@ public class PrimalityTester {
             if(i%2==0) {
                 continue;
             }
-            // assume i is prime unless proven otherwise
-            boolean prime = true;
-            double  sqrt   = Math.sqrt((double)i);
-            // test multiplicands up to the sqrt of i (multiplication mirroring)
-            // not necessary to test for multiplicand 1 as this is true for primes
-            for(int j = 3; j <= floor(sqrt); j+=2) {
-                double mltplr = (double)i/(double)j;
-                // test if the multiplier is an integer 
-                if(floor(mltplr) == mltplr) {
-                    // i cannot be prime -- prime condition is factors [1, i] only
-                    // non-primality found -- no further checks necessary
-                    prime = false;
-                    break;
-                }
-            }
-            if(prime) {
+            if(isPrime(i)) {
                 // i passes primality check -- i is prime, add to container
                 primes.add(i);
                 if(k == 1 || k == 4) {
@@ -57,6 +42,29 @@ public class PrimalityTester {
         }
         return primes;
     }
-    //primes(int,int)===========================================================
+    //listPrimes(int,int)=======================================================
+    /**
+     * Determines if the passed parameter is a prime number or not.
+     * @param  n  number being tested for primality
+     * @return true if n is prime, false if n is composite
+     */
+    public boolean isPrime(int n) {
+        // assume i is prime unless proven otherwise
+            boolean prime  = true;
+            double  sqrt   = Math.sqrt((double)n);
+            // test multiplicands up to the sqrt of i (multiplication mirroring)
+            // not necessary to test for multiplicand 1 as this is true for primes
+            for(int i = 3; i <= floor(sqrt); i+=2) {
+                double mltplr = (double)n/(double)i;
+                // test if the multiplier is an integer 
+                if(floor(mltplr) == mltplr) {
+                    // i cannot be prime -- prime condition is factors [1, i] only
+                    // non-primality found -- no further checks necessary
+                    return false;
+                }
+            }
+            return true;
+    }
+    //isPrime(int)==============================================================
 }
 //PrimalityTester.java==========================================================

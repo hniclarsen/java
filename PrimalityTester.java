53c5f63a4cc6848cdac2b1669e9a5e130157276a
Heather N. Larsen <48229001+hniclarsen@users.noreply.github.com>
2019-03-06 20:08:19 -0700

    Add files via upload

--- PrimalityTester.java
+++ PrimalityTester.java
@@ -11,31 +11,29 @@ public class PrimalityTester {
     /**
      * Returns a list of prime numbers between a declared lower and upper bound
      * -- use ".size" with returned value to count number of discovered primes.
-     * @param  lo  lower-bound integer
-     * @param  hi  upper-bound integer
+     * @param  lo  inclusive lower-bound integer
+     * @param  hi  inclusive upper-bound integer
      * @return the list of primes between lo and hi
      */
     public List primes(int lo, int hi) {
         // initialize a linked-list storage container for discovered primes
         List primes = Collections.synchronizedList(new LinkedList());
         // check each integer between val lo and hi for primality
-        int k = 1;
+        int k = 1;                              // for prime pattern incrementation
+        if(lo<=1) { lo = 2; }                   // n<2 is not prime
+        if(lo<=2 && hi>=2) { primes.add(2); }   // 2 is prime
+        if(lo<=3 && hi>=3) { primes.add(3); }   // 3 is prime
         for(int i = lo; i <= hi; i+=k) {
-            // skip 1 and 0 -- these are not prime
-            if(i <= 1) {
-                continue;
-            }
-            // 1 < x <= 3 are prime
-            if(i <= 3) {
-                primes.add(i);
+            // remove even numbers
+            if(i%2==0) {
                 continue;
             }
             // assume i is prime unless proven otherwise
             boolean prime = true;
-            double  sqr   = Math.sqrt((double)i);
+            double  sqrt   = Math.sqrt((double)i);
             // test multiplicands up to the sqrt of i (multiplication mirroring)
             // not necessary to test for multiplicand 1 as this is true for primes
-            for(int j = 2; j <= floor(sqr); ++j) {
+            for(int j = 3; j <= floor(sqrt); j+=2) {
                 double mltplr = (double)i/(double)j;
                 // test if the multiplier is an integer 
                 if(floor(mltplr) == mltplr) {
